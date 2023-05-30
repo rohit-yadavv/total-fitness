267afd44d215542dc41bbb7d5687e5d69d7e4e1c
Rohit Yadav <126584837+Rohit-Yadavv@users.noreply.github.com>
2023-05-30 07:29:21 +0530

    bug fix

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,33 +1,40 @@
-import React from 'react'
-import Banner2 from '../assets/10837978_19333425.jpg'
-import { Box, Stack, Typography, Button } from '@mui/material';
+import React from "react";
+import Banner2 from "../assets/10837978_19333425.jpg";
+import { Box, Typography, Button } from "@mui/material";
 function HeroBanner() {
   return (
-    <Box sx={{
-      // mt: { lg: '100px', xs: '50px' },
-      mt: '70px',
-      ml: { sm: '50px' }
-    }} position='relative' p='20px'>
-
+    <Box
+      sx={{
+        // mt: { lg: '100px', xs: '50px' },
+        mt: "70px",
+        ml: { sm: "50px" },
+      }}
+      position="relative"
+      p="20px"
+    >
       <Typography>
-        <span className='buildText'>BUILD</span><span className='yourBodyText'>YOUR BODY</span>
+        <span className="buildText">BUILD</span>
+        <span className="yourBodyText">YOUR BODY</span>
       </Typography>
       <Typography>
-        <span className='transformText'>TRANSFORM </span><span className='yourLifeText'>YOUR LIFE</span>
+        <span className="transformText">TRANSFORM </span>
+        <span className="yourLifeText">YOUR LIFE</span>
       </Typography>
       {/* </Typography> */}
-      <Typography
-        fontSize='22px'
-        lineHeight='35px'
-        mb={4}>
+      <Typography fontSize="22px" lineHeight="35px" mb={4}>
         Check Out the Most Effective Exercises
       </Typography>
 
-      <Button variant="contained" color='error' sx={{ backgroundColor: '#ff2625', padding: '15px' }}
+      <Button
+        variant="contained"
+        color="error"
+        sx={{ backgroundColor: "#ff2625", padding: "15px" }}
         onClick={() => {
-          window.scrollTo({ top: 1000, left: 100, behavior: 'smooth' });
+          window.scrollTo({ top: 1000, left: 100, behavior: "smooth" });
         }}
-      >Explore Exercise</Button>
+      >
+        Explore Exercise
+      </Button>
 
       <Typography
         fontWeight={600}
@@ -35,14 +42,17 @@ function HeroBanner() {
         color="#ab8da7"
         sx={{
           opacity: 0.15,
-          display: { lg: 'block', xs: 'none' }
+          display: { lg: "block", xs: "none" },
         }}
-        fontSize='200px' textAlign='center' marginTop='100px'>EXERCISES</Typography>
-      <img src={Banner2} alt="Banner" className='hero-banner-img' />
-
-    </Box >
-  )
+        fontSize="200px"
+        textAlign="center"
+        marginTop="100px"
+      >
+        EXERCISES
+      </Typography>
+      <img src={Banner2} alt="Banner" className="hero-banner-img" />
+    </Box>
+  );
 }
 
-export default HeroBanner
-
+export default HeroBanner;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Stack } from "@mui/material";
 import Logo from "../assets/logo-removebg-preview (1).png";
 export default function Navbar() {
@@ -21,18 +21,6 @@ export default function Navbar() {
           style={{ width: "250px", height: "100px", margin: "0 20px" }}
         />
       </Link>
-      {/* <Stack
-                direction="row"
-                gap='30px'
-                fontSize="24px"
-                alignItems="flex-end"
-            >
-
-                <Link to="/" className= 'NavLink'
-                 style={{ textDecoration: "none", color: "#3a1212"}}
-                 >Home</Link>
-                <Link to="#exercise" className= 'NavLink' style={{ textDecoration: "none", color: "#3a1212" }}>Exercise</Link>
-            </Stack> */}
     </Stack>
   );
 }
