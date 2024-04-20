99bf508fc2b0ed91284941c66c103e0fa63b66e5
Rohit Yadav <rk4740779@gmail.com>
2024-04-20 17:48:10 +0530

    fixed prettier formatting

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
-// import './App.css';
-// import React from 'react'
-// import { Route, Routes} from 'react-router-dom'
-// import Box from '@mui/material/Box';
-// import Navbar from './components/Navbar';
-// import Footer from './components/Footer';
-// import ExerciseDetails from './pages/ExerciseDetails';
-// import Home from './pages/Home';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
 
-// function App() {
-//   return (
-//     <Box width='400px' sx={{ width: { xl: "1488px" }}} m='auto'>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element = {<Home />}/>
-//         <Route path="/exercise/:id" element = {<ExerciseDetails />}/>
-//         </Routes>
-//         <Footer />
-//     </Box>
-//   );
-// }
-
-// export default App;
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Box } from '@mui/material';
-
-import './App.css';
-import ExerciseDetails from './pages/ExerciseDetails';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import "./App.css";
+import ExerciseDetails from "./pages/ExerciseDetails";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => (
-  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+  <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
     <Navbar />
     <Routes>
       <Route path="/" element={<Home />} />
